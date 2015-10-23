754246eb01687b9875e6e2ed5528c36b15808079
Tim <timrobertson0122@gmail.com>
2015-10-23 22:19:27 +0200

    errors with ngMessage on Forms and form validation in Ionic

---
+++ www/js/controllers/authentication.js
@@ -0,0 +1,41 @@
+var app = angular.module('mealtrack.controllers.authentication', []);
+
+/*********************************************************************
+ * LoginCtrl
+ *********************************************************************/
+app.controller('LoginCtrl', function ($scope, $state, AuthService) {
+
+	$scope.formData = {
+		"email": "",
+		"password": ""
+	};
+
+	$scope.login = function (form) {
+		console.log("LoginCtrl::login");
+		if (form.$valid) {
+
+		} else {
+			console.log("Invalid form");
+		}
+		//TODO
+	};
+
+});
+
+/*********************************************************************
+ * SignupCtrl
+ *********************************************************************/
+app.controller('SignupCtrl', function ($scope, $state, AuthService) {
+
+	$scope.formData = {
+		"name": "",
+		"email": "",
+		"password": ""
+	};
+
+	$scope.signup = function () {
+		console.log("SignupCtrl::signup");
+		//TODO
+	};
+
+});
